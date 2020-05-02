8cbe5994d3175e2daedad70b46edb6cff85b17a9
Ankit Gupta <argankit@gmail.com>
2020-05-02 17:42:02 +0530

    styling to component

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,7 +2,11 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
 
 @Component({
     selector: 'event-thumbnail',
-    templateUrl: './event-thumbnail.component.html'
+    templateUrl: './event-thumbnail.component.html',
+    styles: [`
+    .pad-left { padding-left: 10px; }
+    .well div { color: #bbb; }
+    `]
 })
 
 export class EventThumbnailComponent {
