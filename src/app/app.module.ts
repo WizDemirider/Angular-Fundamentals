79d86c68d19885094162a54fe825fe70a45267e1
Ankit Gupta <argankit@gmail.com>
2020-05-03 14:33:37 +0530

    cleaning up imports with index.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver,
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavbarComponent } from './navbar/navbar.component'
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './global-common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/event-list-resolver.service';
 
 @NgModule({
   declarations: [
---
+++ src/app/events/index.ts
@@ -0,0 +1,7 @@
+export * from './create-event/create-event.component'
+export * from './event-thumbnail.component'
+export * from './event-list-resolver.service'
+export * from './events-list.component'
+export * from './shared/event.service'
+export * from './event-details/event-route-activator.service'
+export * from './event-details/event-details.component'
